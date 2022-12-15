cd2661d6729ebd8ff51c66ec31cfcdf0c5e0513b
Adwin Ying <adwinying@gmail.com>
2022-12-15 10:39:55 +0900

    Display basic map UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,47 @@
-import { useState } from "react";
+import { CoordinateKey, MapTerrain } from "@/game/map";
+import { config as gameConfig } from "@/game/state";
+import { useGame } from "@/useGame";
 
-import reactLogo from "@/assets/react.svg";
+export default function App() {
+  const gameMap = useGame(({ map }) => map);
+  const spriteMap = useGame(({ computed }) => computed.spriteMap);
 
-export default function App({ name = "world" }: { name?: string }) {
-  const [count, setCount] = useState(0);
+  const cellColor = {
+    [MapTerrain.Wall]: "bg-base-content dark:bg-neutral",
+    [MapTerrain.Floor]: "bg-base-100 dark:bg-base-content",
+    player: "bg-blue-500",
+    health: "bg-green-700",
+    monster: "bg-red-500",
+    weapon: "bg-yellow-500",
+    exit: "bg-purple-700",
+    boss: "bg-red-900",
+  };
 
   return (
-    <div className="pt-32 text-center">
-      <div className="mb-3 flex justify-center space-x-12">
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img
-            src="/vite.svg"
-            className="w-48 hover:opacity-50"
-            alt="Vite logo"
-          />
-        </a>
-        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-          <img
-            src={reactLogo}
-            className="w-48 hover:opacity-50"
-            alt="React logo"
-          />
-        </a>
-      </div>
-      <h1 className="text-2xl">Hello {name}</h1>
-      <div className="card mx-auto my-3 w-96 bg-base-200">
-        <div className="card-body">
-          <button
-            className="btn-primary btn"
-            type="button"
-            onClick={() => setCount((c) => c + 1)}
-          >
-            count is {count}
-          </button>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test HMR
-          </p>
-        </div>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="min-h-screen text-center">
+      <h1 className="py-8 text-4xl font-bold">Roguelike</h1>
+
+      <table className="mx-auto">
+        <tbody>
+          {Array.from(Array(gameConfig.mapHeight).keys()).map((y) => (
+            <tr key={y}>
+              {Array.from(Array(gameConfig.mapWidth).keys()).map((x) => (
+                <td
+                  key={x}
+                  className={`h-2 w-2 ${
+                    cellColor[
+                      spriteMap.get(CoordinateKey.fromCoor({ x, y }))?.type ??
+                        (gameMap.get(
+                          CoordinateKey.fromCoor({ x, y })
+                        ) as MapTerrain)
+                    ]
+                  }`}
+                />
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
