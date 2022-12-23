bbc0e6319765705a52819c602b354e9d2ed4853a
Adwin Ying <adwinying@gmail.com>
2022-12-23 23:49:02 +0900

    Add immer package

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,11 @@
+import { enableMapSet } from "immer";
 import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "@/App";
 import "@/index.css";
+
+enableMapSet();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
