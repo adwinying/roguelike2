e067bee6c9cf9a7b25859b7f8a8d78156c931f7d
Adwin Ying <adwinying@gmail.com>
2022-12-26 15:52:24 +0900

    Implement player movement feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,40 @@
+import { useEffect } from "react";
+
 import Map from "@/Map";
+import useGame from "@/useGame";
 
 export default function App() {
+  const { triggerMove } = useGame();
+
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      switch (event.key) {
+        case "ArrowUp":
+        case "w":
+          triggerMove("up");
+          break;
+        case "ArrowDown":
+        case "s":
+          triggerMove("down");
+          break;
+        case "ArrowLeft":
+        case "a":
+          triggerMove("left");
+          break;
+        case "ArrowRight":
+        case "d":
+          triggerMove("right");
+          break;
+        default:
+          break;
+      }
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+
+    return () => window.removeEventListener("keydown", handleKeyDown);
+  }, [triggerMove]);
+
   return (
     <div className="min-h-screen text-center">
       <h1 className="py-8 text-4xl font-bold">Roguelike</h1>
--- src/useGame.ts
+++ src/useGame.ts
@@ -1,19 +1,116 @@
 import { useAtom } from "jotai";
 import { atomWithImmer } from "jotai/immer";
-import { useMemo } from "react";
+import { useCallback, useMemo } from "react";
 
+import { computeMove } from "@/game/action";
+import { CoordinateKey } from "@/game/map";
 import { toSpriteMap } from "@/game/sprite";
 import { initGameState } from "@/game/state";
 
 const gameAtom = atomWithImmer(initGameState());
 
 export default function useGame() {
-  const [{ map: gameMap, sprites }] = useAtom(gameAtom);
+  const [{ map: gameMap, sprites }, updateGameState] = useAtom(gameAtom);
 
   const spriteMap = useMemo(() => toSpriteMap(sprites), [sprites]);
+
+  const resetGame = useCallback(() => {
+    updateGameState((draft) => {
+      const newGame = initGameState();
+      draft.map = newGame.map;
+      draft.sprites = newGame.sprites;
+    });
+  }, [updateGameState]);
+
+  const triggerMove = useCallback(
+    (direction: "up" | "down" | "left" | "right") => {
+      const result = computeMove({ map: gameMap, sprites }, direction);
+
+      if (result === undefined) return;
+
+      if (result.type === "defeat") {
+        resetGame();
+        return;
+      }
+
+      if (result.type === "victory") {
+        resetGame();
+        return;
+      }
+
+      if (result.type === "exit") {
+        resetGame();
+        return;
+      }
+
+      if (result.type === "kill") {
+        updateGameState((draft) => {
+          draft.sprites.monsters.delete(
+            CoordinateKey.fromCoor(result.monsterCoor)
+          );
+        });
+
+        return;
+      }
+
+      if (result.type === "battle") {
+        updateGameState((draft) => {
+          const monsterCoor = CoordinateKey.fromCoor(result.monsterCoor);
+          const targetMonster =
+            draft.sprites.boss?.coordinate &&
+            CoordinateKey.fromCoor(draft.sprites.boss.coordinate) ===
+              monsterCoor
+              ? draft.sprites.boss
+              : draft.sprites.monsters.get(monsterCoor);
+
+          if (targetMonster === undefined)
+            throw new Error("Target monster not found");
+
+          targetMonster.health = result.monsterHealth;
+          draft.sprites.player.health = result.playerHealth;
+        });
+
+        return;
+      }
+
+      if (result.type === "health") {
+        updateGameState((draft) => {
+          draft.sprites.player.coordinate = result.playerCoor;
+          draft.sprites.player.health = result.playerHealth;
+          draft.sprites.healths.delete(
+            CoordinateKey.fromCoor(result.playerCoor)
+          );
+        });
+
+        return;
+      }
+
+      if (result.type === "weapon") {
+        updateGameState((draft) => {
+          draft.sprites.player.coordinate = result.playerCoor;
+          draft.sprites.player.attack = result.playerAttack;
+          draft.sprites.weapon = undefined;
+        });
+
+        return;
+      }
+
+      if (result.type === "movement") {
+        updateGameState((draft) => {
+          draft.sprites.player.coordinate = result.playerCoor;
+        });
+
+        return;
+      }
+
+      throw new Error("Unknown result type");
+    },
+    [gameMap, sprites, updateGameState, resetGame]
+  );
 
   return {
     gameMap,
     spriteMap,
+    triggerMove,
   };
 }
